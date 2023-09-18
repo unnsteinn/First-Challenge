ebd1d8177f878e333512e4b55c19e64539588f84
Unnsteinn Árnason <unnsteinn91@gmail.com>
2023-09-18 14:28:34 +0000

    Add project files.

---
+++ LearnCSharp/WeatherData.cs
@@ -0,0 +1,105 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace LearnCSharp
+{
+    internal class WeatherData
+    {
+        public WeatherData(double temperature, double humidity, Char scale)
+        {
+            Temp = temperature;
+            Hum = humidity;
+            Scale = scale;
+
+
+        }
+
+        private double _temp;
+        public double Temp
+        {
+            get
+            {
+                return _temp;
+            }
+            set
+            {
+                if (value < -60)
+                {
+                    Console.WriteLine("That must be a mistake any number temperature -60 is unrealistic");
+                }
+                if (value > 60)
+                {
+                    Console.WriteLine("That must be a mistake any temperature above 60 is unrealistic");
+                }
+                else
+                {
+                    _temp = value;
+                }
+            }
+        }
+
+        private double _hum;
+        public double Hum
+        {
+            get
+            {
+                return _hum;
+            }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    Console.WriteLine("Please enter a humidity between 0% and 100% ");
+                }
+                else
+                {
+                    _hum = value; 
+                }
+
+            }
+
+        }
+
+        private char _scale;
+        public char Scale
+        {
+            get
+            {
+                return _scale;
+            }
+            set
+            {
+                if (value == 'C' || value == 'F')
+                {
+                    _scale = value;
+                }
+                else
+                {
+                    Console.WriteLine("Thats not a valid Character, please use 'C' or 'F'");
+                }
+            }
+
+        }
+        public void Convert()
+        {
+            if (_scale == 'C')
+            {
+                _temp = (_temp * 9 / 5) + 32;
+                _scale = 'F';
+            }
+            else if (_scale == 'F')
+            {
+                // Convert from Fahrenheit to Celsius
+                _temp = (_temp - 32) * 5 / 9;
+                _scale = 'C';
+            }
+        }
+
+        public void DisplayWeatherInfo()
+        {
+            Console.WriteLine($"Temperature: {_temp} {_scale}");
+            Console.WriteLine($"Humidity: {_hum}%");
+        }
+
+    }
+
+}
---
+++ LearnCSharp/Æfing 1.cs
@@ -0,0 +1,68 @@
+﻿
+
+namespace LearnCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // CHALLENGE 1
+
+
+
+            /*
+            List<int> numbers = new List<int>();
+            int number;
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number (press Enter to stop):");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                if (int.TryParse(input, out number))
+                {
+                    numbers.Add(number);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+
+            if (numbers.Count > 0)
+            {
+                int smallest = numbers.Min();
+                int largest = numbers.Max();
+
+                Console.WriteLine($"Smallest number entered: {smallest}");
+                Console.WriteLine($"Largest number entered: {largest}");
+            }
+            else
+            {
+                Console.WriteLine("No numbers were entered.");
+            }
+        }
+            */
+
+
+            // CHALLENGE 2
+
+            WeatherData w1 = new WeatherData(50,69,'F');
+            Console.WriteLine(w1);
+            w1.DisplayWeatherInfo();
+            w1.Convert();
+            w1.DisplayWeatherInfo();
+
+            WeatherData w2 = new WeatherData(80, 200, 'E');
+            w2.DisplayWeatherInfo();
+
+
+        }
+    }
+}
